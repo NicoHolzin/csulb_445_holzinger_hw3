d38489b7d4b8a4ac0af7ec1a2401f5fc0c0808fb
Nicolas Holzinger <nicoholzin@gmail.com>
2024-09-15 15:07:41 -0700

    v2

--- js/Exercice2.js
+++ js/Exercice2.js
@@ -14,3 +14,33 @@
 // 31 – 60: Sophomore
 // 61 – 90: Junior
 // > 91: Senior
+
+let name = prompt("Please type your name");
+let units = prompt("Please type the number of units completed at college");
+let units_int = Number.parseInt(units);
+
+if (Number.isInteger(units_int) && units_int >= 0) {
+  console.log(`Thank you! You entered ${units_int}, a valid number of units.`);
+
+  let GradeStanding;
+
+  if (units_int <= 30) {
+    GradeStanding = "Freshman";
+  } else if (units_int >= 31 && units_int <= 60) {
+    GradeStanding = "Sophomore";
+  } else if (units_int >= 61 && units_int <= 90) {
+    GradeStanding = "Junior";
+  } else if (units_int >= 91) {
+    GradeStanding = "Senior";
+  }
+
+  console.log(`Hello ${name}\nYour grade standing is ${GradeStanding}`);
+
+} else {
+  console.log(`Sorry, ${units} is not a valid number of units.`);
+}
+
+
+
+
+
