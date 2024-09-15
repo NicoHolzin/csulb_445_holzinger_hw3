b7c6af39d81090ae2471efae42c999656b76a74d
Nicolas Holzinger <nicoholzin@gmail.com>
2024-09-15 15:29:12 -0700

    v4

--- js/Exercice4.js
+++ js/Exercice4.js
@@ -9,3 +9,19 @@
 // • Multiplication
 // • Division
 // • Modulo
+
+let num1;
+let num2;
+while (!Number.isInteger(num1)) {
+  num1 = Number.parseInt(prompt("Type a number 1"));
+}
+while (!Number.isInteger(num2)) {
+  num2 = Number.parseInt(prompt("Type a number 2"));
+}
+
+console.log(`
+  Addition : ${num1} + ${num2} = ${num1+num2}\n
+  Subtraction : ${num1} - ${num2} = ${num1-num2}\n
+  Multiplication : ${num1} * ${num2} = ${num1*num2}\n 
+  Division : ${num1} / ${num2} = ${num1/num2}\n 
+  Modulo : ${num1} % ${num2} = ${num1%num2}\n`)
