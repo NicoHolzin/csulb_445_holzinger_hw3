f4af27c4132e9e25071ade2b15bf5b05b42c7d67
Nicolas Holzinger <nicoholzin@gmail.com>
2024-09-16 13:41:59 -0700

    v7

--- js/Exercice7.js
+++ js/Exercice7.js
@@ -14,19 +14,42 @@
 // One second later: 0h0m0s
 
 let hour;
-while (!Number.isInteger(hour)) {
+while (!Number.isInteger(hour) || hour<0 || hour>=24) {
   hour = Number.parseInt(prompt("Type the hour"));
 }
+console.log(hour);
 
 let minute;
-while (!Number.isInteger(minute)) {
+while (!Number.isInteger(minute)|| minute<0 || minute>=60) {
   minute = Number.parseInt(prompt("Type the minute"));
 }
 
 let second;
-while (!Number.isInteger(second)) {
+while (!Number.isInteger(second)|| second<0 || second>=60) {
   second = Number.parseInt(prompt("Type the second"));
 }
 
+console.log(`Time input : ${hour}h${minute}m${second}s`);
+
+if (second===59) {
+  if (minute===59) {
+    if (hour===23) {
+      hour=0;
+      minute=0;
+      second=0;
+    } else {
+      hour++;
+      minute=0;
+      second=0;
+    }
+  } else {
+    minute++;
+    second=0;
+  }
+} else {
+  second++;
+}
+
+console.log(`One second later: ${hour}h${minute}m${second}s`);
 
 
